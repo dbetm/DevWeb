201761187c85fe0c35ce05cfab71814017e26aec
David BM <davbetm@gmail.com>
2018-02-03 13:27:26 -0600

    Init course about JS

---
+++ Curso JS/Introducción/js/01_sintaxis.js
@@ -0,0 +1,5 @@
+// cuando la ventana haya cargado completamente
+window.onload = function() { // es necesario porque está en el head
+    // Crear una consola
+    console.log("Hola mundo")
+}
---
+++ Curso JS/Introducción/js/02_variables.js
@@ -0,0 +1,28 @@
+
+/* =====================
+VARIABLES, no es obligatorio colocar ;
+===================== */
+
+// Númericas
+var numero = 5;
+console.log("Número: ", numero);
+
+// De texto
+var palabra = "Hola Mundo";
+console.log("Palabra", palabra);
+
+// Booleanas
+var booleana = true;
+console.log("Booleana", booleana);
+
+// Array
+var colores = ["rojo", "amarillo"];
+console.log("Colores: ", colores);
+
+
+// Objetos, object: propiedad y el valor
+var jugo = {Ingrediente1:"fresa",
+            Ingrediente2:"mandarina",
+            "Ingrediente 3":"banano"}
+console.log("Jugo", jugo);
+console.log("Jugo", jugo.Ingrediente1);
